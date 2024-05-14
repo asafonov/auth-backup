370c5f68693e1a60ad7dbf990663e3300f35e698
asafonov <me@asafonov.org>
2024-05-14 07:51:47 +0000

    minor update

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
 const DATA_DIR = './data/';
+const TRANSPORT_EMAIL = 'email';
+const KEY_LENGTH = 6;
+const TRANSPORT = TRANSPORT_EMAIL;
 
 function getDataFilename ($user_id) {
   return DATA_DIR . $user_id;
@@ -11,15 +14,40 @@ function getKeyFilename ($user_id) {
 }
 
 function userExists ($user_id) {
-  isEmail($user_id) && file_exists(getDataFilename($user_id));
+  isTransportFormatted($user_id) && file_exists(getDataFilename($user_id));
 }
 
 function replaceParam ($user_id) {
-  return preg_replace('/[^A-z0-9_\.@\-\+]/i', '', $user_id);
+  if (TRANSPORT === TRANSPORT_EMAIL) {
+    return preg_replace('/[^A-z0-9_\.@\-\+]/i', '', $user_id);
+  }
 }
 
-function isEmail ($email) {
-  return preg_match('/^[A-z0-9_\+\-\.]+@[A-z0-9_\-\.]+\.[A-z0-9]+$/i', $email);
+function isTransportFormatted ($user_id) {
+  if (TRANSPORT === TRANSPORT_EMAIL) {
+    return preg_match('/^[A-z0-9_\+\-\.]+@[A-z0-9_\-\.]+\.[A-z0-9]+$/i', $user_id);
+  }
+}
+
+function genKey() {
+  $max = pow(10, KEY_LENGTH - 1);
+  $rand = rand(0, $max);
+  $len = strlen($rand);
+
+  for ($i = 0; $i < KEY_LENGTH - $len; ++$i) {
+    $rand = "0$rand";
+  }
+
+  return $rand;
+}
+
+function sendKey ($user_id, $key) {
+  if (TRANSPORT === TRANSPORT_EMAIL) {
+    $to = $user_id;
+    $subject = 'key';
+    $message = $key;
+    mail($to, $subject, $message);
+  }
 }
 
 if (isset($_POST['user_id']) && isset($_POST['key'])) {
@@ -28,5 +56,8 @@ if (isset($_POST['user_id']) && isset($_POST['key'])) {
   $user_id = replaceParam($_GET['user_id']);
 
   if (userExists($user_id)) {
+    $key = genKey();
+    sendKey($user_id, $key);
+    saveKey($user_id, $key);
   }
 }
